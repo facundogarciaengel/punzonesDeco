ed32f1683ba9e9ffe19e5fe6f5c6aeaed1a9a5d2
MarianoRada <marianorada.mr@gmail.com>
2025-07-11 15:46:23 -0300

    se acomodaron cards de productos y mapa en contactanos

--- js/scripts.js
+++ js/scripts.js
@@ -1,305 +1,67 @@
+
+
 const categorias = [
   {
-  titulo: "Plegaderas de Metal",
-  contenido: `
-    <div class="col-md-4 mb-4">
-      <div class="card h-100 shadow-sm cta-inner bg-faded">
-        <a data-bs-toggle="modal" data-bs-target="#imgModal" onclick="setModalImage('assets/img/plegadora_acero.jpg')">
-          <div class="img-hover-zoom">
-            <img src="assets/img/plegadora_acero.jpg" class="img-fluid rounded img-producto" alt="Plegadera Acero">
-            <div class="zoom-icon"><i class="bi bi-search"></i></div>
-          </div>
-        </a>
-        <div class="card-body d-flex flex-column" style="font-family: 'Lora', serif;">
-          <h3 class="card-title">Acero Inoxidable AX-01</h3>
-          <p class="card-text">Herramienta artesanal fabricada en acero inoxidable de alta calidad.</p>
-          <ul class="text-start ps-3">
-            <li>120 x 25mm x 2mm</li>
-            <li>Doble punta</li>
-            <li>Ideal para trazar papel, cartulina, cartones finos</li>
-            <li>Su lado más fino es ideal para trabajar cantos, esquinas, cartonaje</li>
-          </ul>
-          <div class="cart-btn mt-auto" onclick="sumarAlCarrito(event)"
-            data-id="Acero Inoxidable"
-            data-nombre="Acero Inoxidable AX-01"
-            data-precio="1000">
-            <img src="assets/carrito.png" alt="Carrito" style="height: 50px;">
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-4 mb-4">
-      <div class="card h-100 shadow-sm cta-inner bg-faded">
-        <a data-bs-toggle="modal" data-bs-target="#imgModal" onclick="setModalImage('assets/img/plegadora_acero.jpg')">
-          <div class="img-hover-zoom">
-            <img src="assets/img/plegadora_acero.jpg" class="img-fluid rounded img-producto" alt="Plegadera Acero">
-            <div class="zoom-icon"><i class="bi bi-search"></i></div>
-          </div>
-        </a>
-        <div class="card-body d-flex flex-column" style="font-family: 'Lora', serif;">
-          <h3 class="card-title">Laton Grande LT-01</h3>
-          <p class="card-text">Ideal para trabajos delicados y detalles finos gracias a su punta resistente.</p>
-          <ul class="text-start ps-3">
-            <li>120 x 20mm x 2mm</li>
-          </ul>
-          <div class="cart-btn mt-auto" onclick="sumarAlCarrito(event)"
-            data-id="Laton Grande"
-            data-nombre="Laton Grande LT-01"
-            data-precio="1000">
-            <img src="assets/carrito.png" alt="Carrito" style="height: 50px;">
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-4 mb-4">
-      <div class="card h-100 shadow-sm cta-inner bg-faded">
-        <a data-bs-toggle="modal" data-bs-target="#imgModal" onclick="setModalImage('assets/img/plegadora_acero.jpg')">
-          <div class="img-hover-zoom">
-            <img src="assets/img/plegadora_acero.jpg" class="img-fluid rounded img-producto" alt="Plegadera Acero">
-            <div class="zoom-icon"><i class="bi bi-search"></i></div>
-          </div>
-        </a>
-        <div class="card-body d-flex flex-column" style="font-family: 'Lora', serif;">
-          <h3 class="card-title">Laton Chica LT-02</h3>
-          <p class="card-text">Herramienta artesanal fabricada en acero inoxidable, ideal para trabajos precisos en encuadernación.</p>
-          <ul class="text-start ps-3">
-            <li>95 x 20mm x 2mm</li>
-          </ul>
-          <div class="cart-btn mt-auto" onclick="sumarAlCarrito(event)"
-            data-id="Laton Chica"
-            data-nombre="Laton Chica LT-02"
-            data-precio="1000">
-            <img src="assets/carrito.png" alt="Carrito" style="height: 50px;">
-          </div>
-        </div>
-      </div>
-    </div>
-  `
-}
-,
-  {
-    titulo: "Plegaderas de Teflon",
-    contenido: `
-      <div class="col-md-4 mb-4">
-        <div class="card h-100 shadow-sm cta-inner bg-faded">
-          <a data-bs-toggle="modal" data-bs-target="#imgModal" onclick="setModalImage('assets/img/plegadora_acero.jpg')">
-            <div class="img-hover-zoom">
-              <img src="assets/img/plegadora_acero.jpg" class="img-fluid rounded img-producto" alt="Herramienta Madera 1">
-              <div class="zoom-icon"><i class="bi bi-search"></i></div>
-            </div>
-          </a>
-          <div class="card-body d-flex flex-column" style="font-family: 'Lora', serif;">
-            <h3 class="card-title">Modelo Abanico TF-02</h3>
-            <p class="card-text">Ideal para tallados y acabados finos en madera.</p>
-            <ul class="text-start ps-3">
-              <li>70 x 50mm x 3mm</li>
-              <li>Angulo  en 90º </li>
-            </ul>
-            <!-- Botón de carrito funcional -->
-          <div class="cart-btn mt-auto" onclick="sumarAlCarrito(event)"
-            data-id="Modelo Abanico" 
-            data-nombre="Modelo Abanico TF-02"
-            data-precio="1000"><img src="assets/carrito.png" alt="Carrito" style="height: 50px;"></div>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4 mb-4">
-        <div class="card h-100 shadow-sm cta-inner bg-faded">
-          <a data-bs-toggle="modal" data-bs-target="#imgModal" onclick="setModalImage('assets/img/plegadora_acero.jpg')">
-            <div class="img-hover-zoom">
-              <img src="assets/img/plegadora_acero.jpg" class="img-fluid rounded img-producto" alt="Herramienta Madera 2">
-              <div class="zoom-icon"><i class="bi bi-search"></i></div>
-            </div>
-          </a>
-          <div class="card-body d-flex flex-column" style="font-family: 'Lora', serif;">
-            <h3 class="card-title">Modelo Clasico TF-03</h3>
-            <p class="card-text">Perfecta para escultura y trabajos de precisión.</p>
-            <ul class="text-start ps-3">
-              <li> 150 x 25 mm x6mm </li>
-              <li>Una punta en triángulo y otra plana con corte  en ángulo de un solo lado</li>
-            </ul>
-            <!-- Botón de carrito funcional -->
-          <div class="cart-btn mt-auto" onclick="sumarAlCarrito(event)"
-            data-id="Modelo Clasico" 
-            data-nombre="Modelo Clasico TF-03"
-            data-precio="1000"><img src="assets/carrito.png" alt="Carrito" style="height: 50px;"></div>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4 mb-4">
-        <div class="card h-100 shadow-sm cta-inner bg-faded">
-          <a data-bs-toggle="modal" data-bs-target="#imgModal" onclick="setModalImage('assets/img/plegadora_acero.jpg')">
-            <div class="img-hover-zoom">
-              <img src="assets/img/plegadora_acero.jpg" class="img-fluid rounded img-producto" alt="Herramienta Madera 1">
-              <div class="zoom-icon"><i class="bi bi-search"></i></div>
-            </div>
-          </a>
-          <div class="card-body d-flex flex-column" style="font-family: 'Lora', serif;">
-            <h3 class="card-title">Modelo Lapiz TL-04</h3>
-            <p class="card-text">Ideal para tallados y acabados finos en madera.</p>
-            <ul class="text-start ps-3">
-              <li>110 x 10mm</li>
-              <li>Una punta redondeada y otra en ángulo
-                de ambos lados</li>
-            </ul>
-            <!-- Botón de carrito funcional -->
-          <div class="cart-btn mt-auto" onclick="sumarAlCarrito(event)"
-            data-id="Modelo Lapiz" 
-            data-nombre="Modelo Lapiz TL-04"
-            data-precio="1000"><img src="assets/carrito.png" alt="Carrito" style="height: 50px;"></div>
-          </div>
-        </div>
-      </div>
-    `
+    titulo: "Plegaderas de Metal",
+    productos: [
+      {
+        id: "Acero Inoxidable",
+        nombre: "Acero Inoxidable AX-01",
+        imagen: "assets/img/plegadora_acero.jpg",
+        alt: "Plegadera Acero",
+        descripcion: "Herramienta artesanal fabricada en acero inoxidable de alta calidad.",
+        detalles: [
+          "120 x 25mm x 2mm",
+          "Doble punta",
+          "Ideal para trazar papel, cartulina, cartones finos",
+          "Su lado más fino es ideal para trabajar cantos, esquinas, cartonaje"
+        ],
+        precio: 1000
+      },
+      {
+        id: "Laton Grande",
+        nombre: "Laton Grande LT-01",
+        imagen: "assets/img/plegadora_acero.jpg",
+        alt: "Plegadera Laton",
+        descripcion: "Ideal para trabajos delicados y detalles finos gracias a su punta resistente.",
+        detalles: ["120 x 20mm x 2mm"],
+        precio: 1000
+      },
+      //... otros productos
+    ]
   },
-  {
-    titulo: "Punzones",
-    contenido: `
-      <div class="col-md-4 mb-4">
-        <div class="card h-100 shadow-sm cta-inner bg-faded">
-          <a data-bs-toggle="modal" data-bs-target="#imgModal" onclick="setModalImage('assets/img/plegadora_acero.jpg')">
-            <div class="img-hover-zoom">
-              <img src="assets/img/plegadora_acero.jpg" class="img-fluid rounded img-producto" alt="Herramienta Madera 1">
-              <div class="zoom-icon"><i class="bi bi-search"></i></div>
-            </div>
-          </a>
-          <div class="card-body d-flex flex-column" style="font-family: 'Lora', serif;">
-            <h3 class="card-title">Micro Punzon MP-01</h3>
-            <p class="card-text">Para perforar cuadernillos de varias hojas, te invitamos sentir la resistencia de la herramienta para evitar el quiebre de su punta.</p>
-            <ul class="text-start ps-3">
-              <li>Mango en madera de algarrobo</li>
-              <li>Punta fina de 0.7 mm</li>
-              <li>Punta esmerilada para no desgarrar el papel mm</li>
-              <li>Capuchón en madera  de Cedro para asegurar la vida de la punta ante posibles caídas</li>
-              <li>Marca engarzada en bronce</li>
-              <li>Para quienes bordan en papel y para quienes buscan un perforado mínimo en sus cuadernillos</li>
-            </ul>
-            <!-- Botón de carrito funcional -->
-          <div class="cart-btn mt-auto " onclick="sumarAlCarrito(event)"
-            data-id="Micro Punzon" 
-            data-nombre="Micro Punzon MP-01"
-            data-precio="1000"><img src="assets/carrito.png" alt="Carrito" style="height: 50px;"></div>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4 mb-4">
-        <div class="card h-100 shadow-sm cta-inner bg-faded">
-          <a data-bs-toggle="modal" data-bs-target="#imgModal" onclick="setModalImage('assets/img/plegadora_acero.jpg')">
-            <div class="img-hover-zoom">
-              <img src="assets/img/plegadora_acero.jpg" class="img-fluid rounded img-producto" alt="Herramienta Madera 2">
-              <div class="zoom-icon"><i class="bi bi-search"></i></div>
-            </div>
-          </a>
-          <div class="card-body d-flex flex-column" style="font-family: 'Lora', serif;">
-            <h3 class="card-title">Punzon Pomo PP-02</h3>
-            <p class="card-text">Recomendamos perforar cuadernillos de hasta 6 hojas de 80gr cada uno.</p>
-            <ul class="text-start ps-3">
-              <li>Mango con diseño en pomo para mayor comodidad de agarre</li>
-              <li>Elección de  madera hasta agotar stock (Algarrobo, Guatambú)</li>
-              <li>Punta a elección en 1.2 mm o 0.7 mm</li>
-              <li>Punta esmerilada para no desgarrar el papel</li>
-              <li>Capuchón en madera  de Cedro para asegurar la vida de la punta ante posibles caídas</li>
-              <li>Marca engarzada en bronce</li>
-            </ul>
-            <!-- Botón de carrito funcional -->
-          <div class="cart-btn mt-auto" onclick="sumarAlCarrito(event)"
-            data-id="Punzon Pomo" 
-            data-nombre="Punzon Pomo PP-02"
-            data-precio="1000"><img src="assets/carrito.png" alt="Carrito" style="height: 50px;"></div>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4 mb-4">
-        <div class="card h-100 shadow-sm cta-inner bg-faded">
-          <a data-bs-toggle="modal" data-bs-target="#imgModal" onclick="setModalImage('assets/img/plegadora_acero.jpg')">
-            <div class="img-hover-zoom">
-              <img src="assets/img/plegadora_acero.jpg" class="img-fluid rounded img-producto" alt="Herramienta Madera 1">
-              <div class="zoom-icon"><i class="bi bi-search"></i></div>
-            </div>
-          </a>
-          <div class="card-body d-flex flex-column" style="font-family: 'Lora', serif;">
-            <h3 class="card-title">Punzon Largo PL-03</h3>
-            <p class="card-text">Ideal para .</p>
-            <ul class="text-start ps-3">
-              <li>Mango con diseño alargado </li>
-              <li>Punta a elección en 1.2 mm o 0.7 mm</li>
-              <li>Punta esmerilada para no desgarrar el papel </li>
-              <li> Capuchón en madera  de Cedro para asegurar la vida de la punta ante posibles caídas</li>
-              <li>Marca engarzada en bronce</li>
-            </ul>
-            <!-- Botón de carrito funcional -->
-          <div class="cart-btn mt-auto" onclick="sumarAlCarrito(event)"
-            data-id="Punzon Largo" 
-            data-nombre="Punzon Largo PL-03"
-            data-precio="1000"><img src="assets/carrito.png" alt="Carrito" style="height: 50px;"></div>
-          </div>
-        </div>
-      </div>
-    `
-  },
-  {
-    titulo: "Otros",
-    contenido: `
-      <div class="col-md-4 mb-4">
-        <div class="card h-100 shadow-sm cta-inner bg-faded">
-          <a data-bs-toggle="modal" data-bs-target="#imgModal" onclick="setModalImage('assets/img/plegadora_acero.jpg')">
-            <div class="img-hover-zoom">
-              <img src="assets/img/plegadora_acero.jpg" class="img-fluid rounded img-producto" alt="Herramienta Madera 1">
-              <div class="zoom-icon"><i class="bi bi-search"></i></div>
-            </div>
-          </a>
-          <div class="card-body d-flex flex-column" style="font-family: 'Lora', serif;">
-            <h3 class="card-title">Cortante de Presicion</h3>
-            <p class="card-text">Cutter Bisturi.</p>
-            <ul class="text-start ps-3">
-              <li>Cuerpo y capuchón en madera de algarrobo</li>
-              <li>Diseño ergonómico, cómo al agarre de la mano</li>
-              <li>Entre el filo del bisturí y el cuerpo el diseño cuenta con un cuello
- para evitar que el dedo se deslice hacía el filo, minimizando posibles lastimaduras.</li>
-              <li>Sistema de ajuste que permite intercambiar filos</li>
-              <li>Admite cualquier filo que su dorso mida 6mm </li>
-            </ul>
-            <!-- Botón de carrito funcional -->
-          <div class="cart-btn mt-auto" onclick="sumarAlCarrito(event)"
-            data-id="Cutter Bisturi" 
-            data-nombre="Cutter Bisturi"
-            data-precio="1000"><img src="assets/carrito.png" alt="Carrito" style="height: 50px;"></div>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4 mb-4">
-        <div class="card h-100 shadow-sm cta-inner bg-faded">
-          <a data-bs-toggle="modal" data-bs-target="#imgModal" onclick="setModalImage('assets/img/plegadora_acero.jpg')">
-            <div class="img-hover-zoom">
-              <img src="assets/img/plegadora_acero.jpg" class="img-fluid rounded img-producto" alt="Herramienta Madera 2">
-              <div class="zoom-icon"><i class="bi bi-search"></i></div>
-            </div>
-          </a>
-          <div class="card-body d-flex flex-column" style="font-family: 'Lora', serif;">
-            <h3 class="card-title">Rodillo para Grabado</h3>
-            <p class="card-text">Presentamos una nueva línea de herramientas  dirigida a grabadores, y artistas gráficos que se inaugura con estos rodillos.
-
-              Vas a poder elegir cilindros engomados nuevos de la medida que necesites o cilindros recuperados y restaurados a nuevo de 5 , 10 o 20 CM
-
-              Todo el cuerpo tiene un diseño original nacido del corazón y la creatividad de Jeremías, para que sea cómodo,  práctico  y aporte durabilidad a la herramienta.
-
-              Trabajado en hierro y madera de cedro. La estructura cuenta con un pie de apoyo para evitar el contacto del rodillo con la superficie de entintado cuando se encuentra en reposo.</p>
-            
-            </ul>
-            <!-- Botón de carrito funcional -->
-          <div class="cart-btn mt-auto" onclick="sumarAlCarrito(event)"
-            data-id="Rodillo para Grabado" 
-            data-nombre="Rodillo para Grabado"
-            data-precio="1000"><img src="assets/carrito.png" alt="Carrito" style="height: 50px;"></div>
-          </div>
-        </div>
-      </div>
-      
-    `
-  }
-
-
+  //... otras categorias
 ];
+
+// Función para generar HTML de productos de forma dinámica
+function generarHTMLProductos(productos) {
+  return productos.map(producto => `
+    <div class="col-12 col-sm-6 col-md-4 mb-4">
+      <div class="card h-100 shadow-sm cta-inner bg-faded">
+        <a data-bs-toggle="modal" data-bs-target="#imgModal" onclick="setModalImage('${producto.imagen}')">
+          <div class="img-hover-zoom">
+            <img src="${producto.imagen}" class="img-fluid rounded img-producto" alt="${producto.alt}">
+            <div class="zoom-icon"><i class="bi bi-search"></i></div>
+          </div>
+        </a>
+        <div class="card-body d-flex flex-column" style="font-family: 'Lora', serif;">
+          <h3 class="card-title">${producto.nombre}</h3>
+          <p class="card-text">${producto.descripcion}</p>
+          <ul class="text-start ps-3">
+            ${producto.detalles.map(detalle => `<li>${detalle}</li>`).join('')}
+          </ul>
+          <div class="cart-btn mt-auto" onclick="sumarAlCarrito(event)"
+            data-id="${producto.id}"
+            data-nombre="${producto.nombre}"
+            data-precio="${producto.precio}">
+            <img src="assets/carrito.png" alt="Carrito" style="height: 50px;">
+          </div>
+        </div>
+      </div>
+    </div>
+  `).join('');
+}
+
 
 let indiceActual = 0;
 
@@ -321,7 +83,7 @@ function actualizarContenido() {
   // Después del fade out, actualizar contenido y título
   setTimeout(() => {
     const categoria = categorias[indiceActual];
-    contenidoDiv.innerHTML = categoria.contenido;
+    contenidoDiv.innerHTML = generarHTMLProductos(categoria.productos);
     tituloSeccion.innerHTML = `<span class="section-heading-lower titulo-lora">${categoria.titulo}</span>`;
 
     // Iniciar fade in
